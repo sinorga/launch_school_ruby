dc1febe487d1893d84338bddc9fbd33b04136f78
orga <orga.shih@gmail.com>
2015-12-17 00:48:54 +0800

    add answers of The Basics chapter

---
+++ the_basics.rb
@@ -0,0 +1,28 @@
+# Exercises 1
+'Orga ' + 'Shih'
+
+# Exercises 2
+thousand = 4444 / 1000
+hundred = 4444 % 1000 / 100
+ten = 4444 % 100 / 10
+one = 4444 % 10
+
+# Exercises 3
+{ start_war: 1880, speed: 1990, titanic: 1993, top_gun: 1984, ai: 2001 }.each_value { |year| puts year }
+
+# Exercises 4
+[1880, 1990, 1993, 1984, 2001].each { |year| puts year }
+
+# Exercises 5
+puts 1 * 2 * 3 * 4 * 5
+puts 1 * 2 * 3 * 4 * 5 * 6
+puts 1 * 2 * 3 * 4 * 5 * 6 * 7
+puts 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8
+
+# Exercises 6
+puts 1.1 * 1.1
+puts 6.1 * 6.1
+puts 3.1 * 3.1
+
+# Exercises 7
+# it may forget end bracket when creating a hash
